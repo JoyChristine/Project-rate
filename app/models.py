7ace83e7884b1ea68858cd7aa80d4bc5dae64376
Joy Christine <joychristin2@gmail.com>
2022-06-11 22:12:31 +0300

    write methods for models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,31 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=20)
     context = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def save_project(self):
+        self.save()
     
+    def delete_project(self):
+        self.delete()
+    
+    @classmethod
+    def search_by_name(cls, search_term):
+        name = cls.object.filter(project__icontains=search_term)
+        return name
+
+    @classmethod
+    def get_all_projects(cls):
+        name=cls.object.all()
+        return name
+
+
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='profile_img/',null=True)
     bio = models.TextField()
@@ -12,3 +33,21 @@ class Profile(models.Model):
     projects = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True)
     email =models.EmailField(max_length=20)
 
+    def __str__(self):
+        return f'{self.author}'
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def search_by_name(cls, search_term):
+        author = cls.object.filter(profile__icontains=search_term)
+        return author
+
+    @classmethod
+    def get_all_profiles(cls):
+        author=cls.object.all()
+        return author
