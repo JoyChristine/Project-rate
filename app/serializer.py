9fdae1fbc270ce02da2263f35815f4d4cb8f8b88
Joy Christine <joychristin2@gmail.com>
2022-06-12 00:49:39 +0300

    add models

--- app/serializer.py
+++ app/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import ProjectsAdded
-
+# convert Django models to JSON objects
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectsAdded
