cdd41da5b5957f70c4c022a3e7a6aec143e99b5f
Joy Christine <joychristin2@gmail.com>
2022-06-12 00:50:11 +0300

    add route to view uer profile

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user/',views.userprofile, name='userprofile'),
     path('api/projects/', views.ProjectList.as_view()),
     path('api/profiles/', views.ProfileList.as_view()),
     
