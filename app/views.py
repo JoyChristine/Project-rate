0014f490a8d0bab6e54ce25e6f4d7b19cd4b3c0a
Joy Christine <joychristin2@gmail.com>
2022-06-12 00:51:57 +0300

    add function to view uer profile page

--- app/views.py
+++ app/views.py
@@ -3,10 +3,24 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib.auth import authenticate,login,logout
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import ProjectsAdded,ProfilesAdded
+from .serializer import ProjectSerializer,ProfileSerializer
+from rest_framework import status
+
+
+
+
 # Create your views here.
 def home(request):
     return render(request, 'all/home.html')
 
+def userprofile(request):
+    return render(request,'all/myprofile.html')
+
+    
 def register(request):
     form = CreateUserForm()
 
@@ -38,3 +52,32 @@ def signin(request):
 def logoutuser(request):
     logout(request)
     return redirect('login')
+
+
+
+# api's
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = ProjectsAdded.objects.all()
+        serializers = ProjectSerializer(all_projects,many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = ProfilesAdded.objects.all()
+        serializers = ProfileSerializer(all_profiles,many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
