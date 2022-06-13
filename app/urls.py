35365bcce1ccda6d42ff80b32a32bb67779c970b
Joy Christine <joychristin2@gmail.com>
2022-06-14 00:33:59 +0300

    add api views + project add views

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from .views import ProjectList, ProjectAdd
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('user/',views.userprofile, name='userprofile'),
-    path('api/projects/', views.ProjectList.as_view()),
-    path('api/profiles/', views.ProfileList.as_view()),
+    # path('api/projects/', views.ProjectList.as_view()),
+    # path('api/profiles/', views.ProfileList.as_view()),
+    path('addprojects/', ProjectAdd.as_view(), name='addproject'),
+    path('', ProjectList.as_view(), name='project_list'),
     
 ]
