9f12753718f6214ead61cf7e9a86771792cc3399
Joy Christine <joychristin2@gmail.com>
2022-06-14 00:23:34 +0300

    add project form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
+from django.forms import modelformset_factory
 from django.contrib.auth.models import User
-
+from .models import Project
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email','password1', 'password2']
+
+ProjectFormSet = modelformset_factory(
+    Project,fields = ("title","context","url")
+)
