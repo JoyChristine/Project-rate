e44c662671b752b7d70be725e6af83ce4e8dc3be
Joy Christine <joychristin2@gmail.com>
2022-06-16 23:52:06 +0300

    add user update profile

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import modelformset_factory
 from django.contrib.auth.models import User
-from .models import Project
+from .models import Project,Rate,Profile
+from django import forms
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email','password1', 'password2']
 
-ProjectFormSet = modelformset_factory(
-    Project,fields = ("title","context","url")
-)
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =  ('profile_pic', 'bio','location')
+
+
+
+class projectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','context','image','url']
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        fields= ['design','usability','content']
+
