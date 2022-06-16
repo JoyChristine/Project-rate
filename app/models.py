3d7f0c7b14d0ee739b388e662a956822994deaa0
Joy Christine <joychristin2@gmail.com>
2022-06-17 00:02:01 +0300

    use cloudinary on img fields, add locations field on profile, add class meta in project

--- app/models.py
+++ app/models.py
@@ -17,11 +17,7 @@ class Profile(models.Model):
     location = models.CharField(max_length=20,null=True, blank=True)
 
 
-    @receiver(post_save,sender=User)
-    def create_user_profile(sender,instance,created,**kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-    
+
     @receiver(post_save,sender=User)
     def save_user_profile(sender,instance,**kwargs):
             instance.profile.save()
