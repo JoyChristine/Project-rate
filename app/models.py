7d7a5aed27ca21f7551213078a7fd3abe8feb662
Joy Christine <joychristin2@gmail.com>
2022-06-16 23:53:48 +0300

    use cloudinary on img fields, add locations field on profile, add class meta in project

--- app/models.py
+++ app/models.py
@@ -1,51 +1,37 @@
 from email.mime import image
 from unicodedata import name
+from urllib.parse import DefragResult
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MaxValueValidator
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
-class Project(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    title = models.CharField(max_length=20)
-    context = models.TextField(max_length=120,blank=True)
-    image=models.ImageField(upload_to='project_img',null=True)
-    date=models.DateTimeField(auto_now_add=True,null=True)
-    rate=models.IntegerField(default=0)
-    url=models.URLField(null=True)
-
-    def __str__(self):
-        return self.title
-
-    def save_project(self):
-        self.save()
-    
-    def delete_project(self):
-        self.delete()
-    
-    @classmethod
-    def search_by_name(cls, search_term):
-        name = cls.object.filter(project__icontains=search_term)
-        return name
-
-    @classmethod
-    def get_all_projects(cls):
-        name=cls.object.all()
-        return name
-
-
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload_to='profile_img/',null=True)
-    bio = models.TextField()
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
-    projects = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    profile_pic = CloudinaryField('image',default='https://res.cloudinary.com/dz275mqsc/image/upload/v1654858776/default_nbsolf.png')
+    bio = models.CharField(max_length=50)
     email =models.EmailField(max_length=20)
+    location = models.CharField(max_length=20,null=True, blank=True)
+
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save,sender=User)
+    def save_user_profile(sender,instance,**kwargs):
+            instance.profile.save()
+
 
     def __str__(self):
-        return f'{self.author}'
+        return self.user.username
 
     def save_profile(self):
-        self.save()
+        self.user
 
     def delete_profile(self):
         self.delete()
@@ -60,12 +46,76 @@ class Profile(models.Model):
         author=cls.object.all()
         return author
 
+class Project(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    title = models.CharField(max_length=20)
+    context = models.CharField(max_length=200,blank=True)
+    image = CloudinaryField('image',default='https://res.cloudinary.com/dz275mqsc/image/upload/v1654858776/default_nbsolf.png')
+    date=models.DateTimeField(auto_now_add=True,null=True)
+    rate=models.IntegerField(default=0)
+    url=models.URLField(null=True)
+    design=models.IntegerField(default=0)
+    usability=models.IntegerField(default=0)
+    content= models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-date']
+     
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+    
+    def delete_project(self):
+        self.delete()
+    
+    @classmethod
+    def search_project(cls, title):
+        return cls.object.filter(title__icontains=title).all()
+       
+
+
+#  @classmethod
+#     def search_profile(cls, name):
+#         return cls.objects.filter(user__username__icontains=name).all()
+
+    @classmethod
+    def get_all_projects(cls):
+        name=cls.object.all()
+        return name
+
+
+
+class Rate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    design = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    content = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    usability = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    project = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user}'
+
+    def save_rate(self):
+        self.save()
+
+    def delete_rate(self):
+        self.delete()
+
+
+
+
+
+
+
 class ProjectsAdded(models.Model):
     name = models.CharField(max_length=40)
     context = models.TextField()
 
 class ProfilesAdded(models.Model):
-    profile_pic = models.ImageField(upload_to='profile_img/',null=True)
+    # profile_pic = models.ImageField(upload_to='profile_img/',blank=True,default='default.png')
     bio = models.TextField()
     author = models.CharField(max_length=10)
     projects = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True)
